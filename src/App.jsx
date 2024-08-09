7354ccbdb43dd5d0de6acbae19831a96180ddfb2
AKSHAY GOPAN <akshaygopan377@gmail.com>
2024-08-10 00:49:01 +0530

    test12

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Router basename="/react-portfolio">
       
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/react-portfolio" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/contact" element={<Contact />} />

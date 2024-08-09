bbbf2403cb433648f6a494e3d559eb8e5460ff13
AKSHAY GOPAN <akshaygopan377@gmail.com>
2024-08-10 00:25:31 +0530

    test8

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,36 +1,35 @@
+import { Link } from 'react-router-dom';
 function Header() {
   return (
     <>
       <nav>
         <div class="flex flex-row justify-center h-20 top-0 left-0 w-full fixed bg-white drop-shadow-sm z-10">
-          {/* <div class="self-center">
-                        <img src="images/logo.png" alt="logo" />
-                    </div> */}
+          
           <div class="flex self-center gap-7">
-            <a
-              href="/"
+            <Link
+              to="/"
               class=" px-3 py-2 text-[17px]  underline-offset-[10px] hover:underline"
             >
               Home
-            </a>
-            <a
-              href="/about"
+            </Link>
+            <Link
+              to="/about"
               class=" px-3 py-2 text-[17px] hover:underline underline-offset-[10px] "
             >
               About
-            </a>
-            <a
-              href="/projects"
+            </Link>
+            <Link
+              to="/projects"
               class=" px-3 py-2 text-[17px] hover:underline underline-offset-[10px]  "
             >
               Project
-            </a>
-            <a
-              href="/contact"
+            </Link>
+            <Link
+              to="/contact"
               class=" px-3 py-2 text-[17px] hover:underline underline-offset-[10px] "
             >
               Contact
-            </a>
+            </Link>
           </div>
         </div>
       </nav>
